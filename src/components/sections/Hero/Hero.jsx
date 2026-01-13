ed6add0bd66c16dc840749af08326131ff5cee1c
DARVIN <darvinstd9749@gmail.com>
2026-01-13 12:31:01 +0530

    Add Hero Section

---
+++ src/components/sections/Hero/Hero.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Hero.css";
+import profileImage from "/N_Darvin.png";
+
+function Hero() {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-content">
+        {/* LEFT */}
+        <div className="hero-left">
+          <h1>
+            Hi, I'm <span>Darvin Padariya</span>
+          </h1>
+
+          <h2>MERN Stack Developer</h2>
+
+          <p>
+            I build modern, responsive, and scalable web applications using
+            React, Node.js, Express, and MongoDB.
+          </p>
+
+          <div className="hero-buttons">
+            <a href="/resume.pdf" target="_blank" className="btn primary">
+              Resume
+            </a>
+            <a href="#contact" className="btn secondary">
+              Contact
+            </a>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="hero-right">
+          <div className="profile-image">
+            <img src={profileImage} alt="Darvin Padariya" />
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
+}
+
+export default Hero;
